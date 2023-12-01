10f6c95108ef32e28e9e6ab06a29a5502c010215
lautarorocchi <62039372+lautarorocchi@users.noreply.github.com>
2023-12-01 01:50:12 -0300

    az

--- services/mail.services.js
+++ services/mail.services.js
@@ -1,15 +1,15 @@
 import sgMail from '@sendgrid/mail'
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-function enviarCorreoVerificacion(correoDestino, token) {
-  const link = `https://tudominio.com/verificar?token=${token}`;
+function enviarCorreoVerificacion(usuario, correoDestino, token) {
+  const link = `https://front-public.vercel.app/verify/${token}`;
 
   const msg = {
     to: 'lautarorocchi@gmail.com',
     from: 'stackux9@gmail.com',
     subject: 'Verificación de Registro',
     text: `Haz clic en el siguiente enlace para verificar tu registro: ${link}`,
-    html: `<p>Haz clic en el siguiente enlace para verificar tu registro: <a href="${link}">${link}</a></p>`,
+    html: `<p>Haz clic en el siguiente enlace para verificar el registro de ${usuario.name}, ${usuario.surname} a tu empresa: <a href="${link}">${link}</a></p>`,
   };
 
   return sgMail.send(msg);
