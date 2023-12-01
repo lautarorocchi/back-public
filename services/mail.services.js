0cbd968559d527572c4c6a220d1f68a00957f695
lautarorocchi <62039372+lautarorocchi@users.noreply.github.com>
2023-12-01 01:18:57 -0300

    Update mail.services.js

--- services/mail.services.js
+++ services/mail.services.js
@@ -1,14 +1,14 @@
 import sgMail from '@sendgrid/mail'
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-function enviarCorreoVerificacion(correoDestino, token, usuario) {
+function enviarCorreoVerificacion(correoDestino, token) {
   const link = `https://front-public.vercel.app/verify/${token}`;
 
   const msg = {
     to: 'lautarorocchi@gmail.com',
     from: 'stackux9@gmail.com',
     subject: 'Validar usuario nuevo para la empresa',
-    text: `Se ha detectado un nuevo usuario en la base de datos de tu empresa. Nombre Completo: ${usuario.name}, ${usuario.surname}. Email: ${usuario.email}
+    text: `Se ha detectado un nuevo usuario en la base de datos de tu empresa. 
     Haz clic en el siguiente enlace para verificar a en la empresa: ${link}`,
     html: `<p>Haz clic en el siguiente enlace para verificar un nuevo usuario: <a href="${link}">${link}</a></p>`,
   };
