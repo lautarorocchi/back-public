e561a576d69b5cca9097ad67fe41b57af62e9add
lautarorocchi <62039372+lautarorocchi@users.noreply.github.com>
2023-11-30 23:14:45 -0300

    Update proyecto.api.routes.js

--- api/routers/proyecto.api.routes.js
+++ api/routers/proyecto.api.routes.js
@@ -2,40 +2,6 @@ import express from 'express'
 import * as proyectoApiControllers from "../controllers/proyecto.api.controllers.js"
 import { isLogin } from '../../middleware/auth.middleware.js'
 import multer from 'multer'
-const sgMail = require('@sendgrid/mail');
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-
-// Configuración del mensaje
-const msg = {
-  to: 'lautaro@rocchi.com',
-  from: 'stackux@gmail.com', // Debe ser un correo verificado en SendGrid
-  subject: 'Asunto del Correo',
-  text: 'Contenido del Correo en Texto Plano',
-  html: '<p>Contenido del Correo con HTML</p>',
-};
-
-
-// Envío del correo
-sgMail
-  .send(msg)
-  .then(() => {
-    console.log('Correo enviado con éxito');
-  })
-  .catch((error) => {
-    console.error('Error al enviar el correo:', error);
-  });
-
-
-/*import { resizeImagenProductos } from '../functions/resizeImagen.js'*/
-
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, './public/img/uploads/productos')
-    },
-    filename: function (req, file, cb) {
-      cb(null, file.originalname)
-    }
-})
 
 const uploadedFile = multer({ storage: storage })
   
@@ -48,7 +14,7 @@ router.route('/api/products/:id')
     .get(proyectoApiControllers.buscarProductosPorEmpresa)    
 
 router.route('/api/productos')
-    .get(proyectoApiControllers.buscarProductos, sgMail)
+    .get(proyectoApiControllers.buscarProductos)
     .post(/*[uploadedFile.any(), resizeImagenProductos],*/ proyectoApiControllers.crearUnProducto)
 
 router.route('/api/productos/:id')
