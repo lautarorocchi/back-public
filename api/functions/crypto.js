c41dd9f3710b392461d83b5b774693ed46d1cda5
lautarorocchi <62039372+lautarorocchi@users.noreply.github.com>
2023-12-12 21:56:32 -0300

    az

--- api/functions/crypto.js
+++ api/functions/crypto.js
@@ -4,10 +4,6 @@ function generateUniqueCode(length = 6) {
   return cryptoRandomString({ length, type: 'alphanumeric' });
 }
 
-/*// Ejemplo de uso:
-const uniqueCode = generateUniqueCode();
-console.log(uniqueCode);*/
-
 export{
     generateUniqueCode
 }
